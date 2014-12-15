5008c9fb421b4e529e6a9cb22883ad3565d4c21a
Linda Nichols <lynnaloo@gmail.com>
2014-12-15 21:50:42 +0000

    refactoring to work with browserify

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      react: {
+        files: ['views/**/*.js', 'Gruntfile.js'],
+        tasks: ['browserify', 'jshint']
+      }
+    },
+    browserify: {
+      options: {
+        transform: [ require('grunt-react').browserify ]
+      },
+      client: {
+        src: ['views/**/*.js', 'views/**/*.jsx', 'models/**/*.js'],
+        dest: 'public/js/app.built.js'
+      }
+    },
+    jshint: {
+      all: ['views/**/*.js'],
+      options: {
+        newcap: false
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jsxhint');
+  grunt.registerTask('default', ['browserify']);
+
+};
