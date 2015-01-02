bb9ea109dd443c9d7e3c447b18310a1ee28c66a7
Steve Hackbarth <stephenhackbarth@gmail.com>
2015-01-02 16:56:29 -0500

    cleanup

--- views/donut-chart-d3.js
+++ views/donut-chart-d3.js
@@ -1,16 +1,12 @@
 var d3Chart = {},
-  c3 = require('c3'),
   chart,
-  d3 = require('d3'),
+  c3 = require('c3'),
   _ = require('lodash');
 
-
 d3Chart.create = function (el, props, state) {
-
   chart = c3.generate({
     data: {
-      columns: [
-      ],
+      columns: [],
       bindto: ".chart",
       type : 'donut'
     },
@@ -19,12 +15,6 @@ d3Chart.create = function (el, props, state) {
     }
   });
   this.update(el, state);
-    /*
-    d3.select(".chart")
-      .style('min-height', props.height);
-
-  this.update(el, state);
-  */
 };
 
 d3Chart.update = function (el, state) {
@@ -35,20 +25,6 @@ d3Chart.update = function (el, state) {
     columns: data,
     unload: null
   });
-
-  /*
-  // http://bost.ocks.org/mike/bar/
-  var x = d3.scale.linear()
-    .domain([0, d3.max(_.map(state.data, function (datum) { return datum.total }))])
-    .range([0, 420]);
-
-  d3.select(".chart")
-    .selectAll("div")
-      .data(state.data)
-    .enter().append("div")
-      .style("width", function (d) { return x(d) * 40 + "px"; })
-      .text(function (d) { return d.key + ": " + d.total; });
-  */
 };
 
 d3Chart.destroy = function (el) {
